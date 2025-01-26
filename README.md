a0bfea583120bb058a63bb3b848657ee1cecd2d5
gurasan0110 <gurasan0110@icloud.com>
2025-01-27 02:48:00 +0900

    Create project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_internationalization_practice
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/gurasan0110/flutter_internationalization_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gurasan0110.flutter_internationalization_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
